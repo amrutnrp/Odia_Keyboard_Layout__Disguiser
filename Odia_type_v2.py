0cd9ca0403e8a337066683b3d7f04e98ac42725f
amrutnrp <60325062+amrutnrp@users.noreply.github.com>
2021-09-18 14:02:16 +0530

    emphasis solved
    need to implement stack

--- Odia_type_v2.py
+++ Odia_type_v2.py
@@ -4,122 +4,9 @@ Created on Fri Sep 17 02:37:17 2021
 
 @author: amrutnp
 """
-
-Odia_data2 = []
-
-# open file and read the content in a list
-with open('Odia_data.txt', 'r') as filehandle:
-    for line in filehandle:
-        # remove linebreak which is the last character of the string
-        currentPlace = line[:-1]
-
-        # add item to the list
-        Odia_data2.append(currentPlace)
-        
-# DEFINE DICTIONARIES
-Odia_data_3= []
-for item in Odia_data2:
-    Odia_data_3.append(item.split(','))
-
-Interpreted_LUT= []
-
-master_Odia_LUT = ['']*len(Odia_data_3 [3])
-master_gen_LUT = ['']*len(Odia_data_3 [9])
-for j,i in enumerate (Odia_data_3 [3]):
-    master_Odia_LUT [j] = chr(int (i,16))
-
-for j,i in enumerate (Odia_data_3 [9]):
-    if 'x' in i:
-        master_gen_LUT [j] = chr(int (i,16))
-    else:
-        master_gen_LUT [j] = i
-        
-for i in [4,5,6,7,8, 10,11,12,13, 15, 16]:
-    
-    temp_LUT = []
-    for k_item in Odia_data_3[i]:
-        k= k_item.split('*')
-        k_str= ''
-        for j_item in k:
-            j = int (j_item)
-            if j >0:
-                k_str= k_str + master_Odia_LUT [j-1]
-            else:
-                k_str = k_str + master_gen_LUT [-j-1]
-        temp_LUT.append(k_str)
-    Interpreted_LUT  .append(temp_LUT) 
-
-#empasis_exclusion_list = Interpreted_LUT [5]
-
-
-valid_od_char = Interpreted_LUT [1]+Interpreted_LUT [2]+Interpreted_LUT [3]
-valid_od_char_empasized = Interpreted_LUT [3]
-valid_od_char_deEmpasized = Interpreted_LUT [5]+Interpreted_LUT [7]
-del currentPlace, Odia_data2, temp_LUT, master_gen_LUT, line
-del k_str, k_item, j_item, j, k, i, item, filehandle, 
-Numbers_map          = dict(zip(Odia_data_3[2], Interpreted_LUT [0]))  # dict(zip(keys, values))
-Normal_map           = dict(zip(Odia_data_3[1], Interpreted_LUT [1]))  
-Shift_map            = dict(zip(Odia_data_3[0], Interpreted_LUT [3])) 
-
-Emphasis_map         = dict(zip(Interpreted_LUT [1], Interpreted_LUT [2])) 
-Double_Emphasis_map   = dict(zip(Interpreted_LUT [1], Interpreted_LUT [4])) 
-Shift_Emphasis_map   = dict(zip(Interpreted_LUT[3], Interpreted_LUT [4])) 
-
-De_Emphasis_map   = dict(zip(Interpreted_LUT [5]+Interpreted_LUT [7], Interpreted_LUT[6]+Interpreted_LUT [8])) 
-
-
-juktakhar_emphasis =  dict(zip(Interpreted_LUT[9], Interpreted_LUT [10])) 
-
-
-control_inp_types = [
-"'\\x01'",     #A
-"'\\x02'",     #B 
-"'\\x03'",     #C 
-"'\\x04'",     #D 
-"'\\x05'",     #E 
-"'\\x06'",     #F 
-"'\\x07'",     #G 
-"'\\x08'",     #H
-"'\\t'"  ,     #I
-"'\\n'"  ,     #J  
-"'\\x0b'",     #K  
-"'\\x0c'",     #L  
-"'\\r'" ,       #M
-"'\\x0e'",     #N 
-"'\\x0f'",     #O 
-"'\\x10'",     #P 
-"'\\x11'",     #Q 
-"'\\x12'",     #R 
-"'\\x13'",     #S  
-"'\\x14'",     #T 
-"'\\x15'",     #U
-"'\\x16'",     #V 
-"'\\x17'",     #W
-"'\\x18'",     #X 
-"'\\x19'",     #Y
-"'\\x1a'"     #Z 
-    ]
-
-control_based_juktakshar = {}
-b= Odia_data_3[14]
-for j,i in enumerate(b):
-    if not i[0].isalpha():
-        i_lists= i.split('*')
-        i_str= ''
-        for k in i_lists:
-            k_num = int (k)
-            if k_num>0:
-                i_str=i_str + master_Odia_LUT [k_num-1]
-            else:
-                raise KeyboardInterrupt
-        control_based_juktakshar [control_inp_types [j]] = i_str
-         
-empasis_exclusion_list = [' ', '-'] + Odia_data_3[2]# + list (control_based_juktakshar.keys())     
-valid_EN_char = Odia_data_3[0]+Odia_data_3[1]+Odia_data_3[2] +   list (control_based_juktakshar.keys())     
-#del Interpreted_LUT, Odia_data_3    , master_Odia_LUT    
-from tkinter import *
 import tkinter
 root = tkinter.Tk()
+from Import_odia_process import  *
 print('Programm starting...')
 chr_pressed =None
 flag=0
@@ -128,7 +15,7 @@ flag=0
 valid_emphasis = False
 valid_double_emphasis= False
 last_input = ''
-last_od_type = ''
+last_od_type = '%'
 valid_de_emphasis = False
 
 
@@ -136,7 +23,7 @@ valid_de_emphasis = False
 #=====================================================================================
 def key(event):
     kp = repr(event.char)
-    if len(kp)==2 :
+    if len(kp)==2 or len (kp) == 0 :
         return    
     
     global chr_pressed, valid_emphasis ,last_input, valid_double_emphasis, last_od_type, valid_de_emphasis
@@ -145,7 +32,7 @@ def key(event):
     text_box.config(state="normal")
     #print ("pressed", kp,  len(kp)) #repr(event.char))
 
-    if (kp == "'\\t'" or kp == "'p'" ) and  valid_emphasis== True: #space
+    if (kp == "'\\t'" or kp == "']'" ) and  valid_emphasis== True: #space
         last_len= len(last_od_type)
         text_box.delete('end-'+str(last_len+1)+'c', 'end-1c')
         od_uni = Superset_empasis_map[last_od_type]            
@@ -153,10 +40,13 @@ def key(event):
         text_box.insert(tkinter.END, od_chr [1:-1])
         
         valid_emphasis = False  
-        valid_double_emphasis= True
+        if last_od_type in single_emphasis_exclusion_list:
+            valid_double_emphasis= False
+        else:
+            valid_double_emphasis= True
         valid_de_emphasis = True
         
-    elif (kp == "'\\t'" or kp == "'p'" ) and valid_double_emphasis== True:  # double space
+    elif (kp == "'\\t'" or kp == "']'" ) and valid_double_emphasis== True:  # double space
         last_len= len(last_od_type)
         text_box.delete('end-'+str(last_len+1)+'c', 'end-1c')
         
@@ -174,18 +64,21 @@ def key(event):
         text_box.insert(tkinter.END, last_od_type )
         last_input= kp
         valid_de_emphasis = False
+        valid_double_emphasis= False
         if kp in empasis_exclusion_list :
             valid_emphasis = False
         else:
             valid_emphasis = True
     else:
         valid_emphasis = False
+        valid_double_emphasis= False        
         if len (kp) == 3:
             text_box.insert(tkinter.END, kp[1])
+            valid_de_emphasis = False
         else:
             if kp == "'\\x08'" : # Backspace
                 last_od_type = text_box.get('end-2c', 'end-1c')
-                if last_od_type in valid_od_char_deEmpasized:
+                if valid_de_emphasis and last_od_type in valid_od_char_deEmpasized:
                     text_box.delete('end-2c', 'end-1c')
                     od_uni = De_Emphasis_map[last_od_type]
                     od_chr = repr(od_uni)
